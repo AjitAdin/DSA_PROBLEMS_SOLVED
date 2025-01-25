c574ecc5ef75e6042e4a0d89b2e8783eebab907c
AjitAdin <ajitadin03@gmail.com>
2025-01-26 00:56:26 +0530

    updt

--- NumberSystem/src/Main.java
+++ NumberSystem/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String args[]){
-        System.out.println("HELLO");
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '*') {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+        int r=count1-count2;
+        System.out.println(r);
     }
+
 }
