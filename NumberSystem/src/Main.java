a95568410dec414094cbbac997874659dc581286
AjitAdin <ajitadin03@gmail.com>
2025-01-30 23:49:16 +0530

    upt

--- NumberSystem/src/Main.java
+++ NumberSystem/src/Main.java
@@ -1,41 +1,24 @@
 import java.util.*;
-
-class Main{
+public class Main{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
 
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-
-        int mat[][]=new int[r][c];
-
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                mat[i][j]=sc.nextInt();
-            }
-        }
-
-
+        int E[]={3,5,2,0};
+        int L[]={0,2,4,4};
         int max=0;
-        int row=0;
-        int count;
-
-        for(int i=0;i<r;i++){
-            count=0;
-            for(int j=0;j<c;j++){
-                if(mat[i][j]==1){
-
-                    count++;
-
-
-                }
+        int cur=0;
+        for(int i=0;i<t;i++){
+            int temp=cur+E[i];
+            cur=temp-L[i];
+            if(cur>max){
+                max=cur;
 
             }
-            if(max<count){
-                max=count;
-                row=i+1;
-            }
+
         }
-        System.out.println(row);
+        System.out.println(max);
+
+
     }
 }
