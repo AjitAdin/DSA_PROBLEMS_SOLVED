856fddefce1e0c0faec02ae5401b1e580b75c534
AjitAdin <ajitadin03@gmail.com>
2025-02-14 07:53:33 +0530

    pattern update

--- PATTERNS/code/src/p2.java
+++ PATTERNS/code/src/p2.java
@@ -1,2 +1,16 @@
-public class p2 {
+class p2 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            // Printing spaces (decreasing)
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("#");
+            }
+            // Printing stars (increasing)
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
 }
