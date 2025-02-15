b27b04764361061ca028f0135d7b1db123e74bea
AjitAdin <ajitadin03@gmail.com>
2025-02-15 20:04:30 +0530

    updt

--- PATTERNS/code/src/DiamondPattern.java
+++ PATTERNS/code/src/DiamondPattern.java
@@ -14,15 +14,12 @@ public class DiamondPattern  {
 
             System.out.println();
         }
-        for(int i=n-1;i>=0;i--){
-
-
-            for(int k=0;k<=(2*i);k++){
-                System.out.print("*");
-
-            }
-            for(int j=n-1;j>=i;j--){
+        for(int i=n-1;i>0;i--){
+            for(int k=i;k<=n-1;k++){
                 System.out.print(" ");
+            }
+            for(int j=1;j<2*i;j++){
+                System.out.print("*");
             }
 
             System.out.println();
@@ -31,36 +28,3 @@ public class DiamondPattern  {
 }
 
 
-public class DiamondPattern {
-    public static void main(String[] args) {
-        int n = 5;
-
-        // Upper half
-        for (int i = 0; i < n; i++) {
-
-            for (int j = n - i - 1; j > 0; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= (2 * i); k++) {
-                System.out.print("*");
-            }
-
-            System.out.println();
-        }
-
-        // Lower half
-        for (int i = n - 2; i >= 0; i--) {  // Start from n-2 to avoid duplicate middle row
-
-            for (int j = n - i - 1; j > 0; j--) { // Fix spacing in lower part
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= (2 * i); k++) {
-                System.out.print("*");
-            }
-
-//
-
-            System.out.println();
-        }
-    }
-}
