2bebdb4c648b5ef944554d3a3d37c229bd68d5f9
AjitAdin <ajitadin03@gmail.com>
2025-02-19 21:18:22 +0530

    updt

--- TCSCODE/ARRAYS/src/PushEmptyPacketstotheEndoftheArray.java
+++ TCSCODE/ARRAYS/src/PushEmptyPacketstotheEndoftheArray.java
@@ -19,3 +19,30 @@ class PushEmptyPacketstotheEndoftheArray {
         System.out.println(Arrays.toString(temp));
     }
 }
+
+
+//Method 2
+//import java.util.*;
+//class Main {
+//    public static void main(String[] args) {
+//        int arr[] = {2, 0, 3, 0, 5};
+//        int temp[] = new int[arr.length];
+//        int index = 0;
+//
+//        System.out.println("Original array: " + Arrays.toString(arr));
+//
+//        // Copy non-zero elements to temp array
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] != 0) {
+//                temp[index++] = arr[i];
+//            }
+//        }
+//
+//        // Optional: Fill remaining positions in temp with zeros (this step is not strictly necessary as the array is already initialized with zeros)
+//        for (int i = index; i < arr.length; i++) {
+//            temp[i] = 0;
+//        }
+//
+//        System.out.println("Array after removing zeros: " + Arrays.toString(temp));
+//    }
+//}
