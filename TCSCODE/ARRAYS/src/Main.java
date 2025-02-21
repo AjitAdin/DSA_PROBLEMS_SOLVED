0a8eda1a833608515c5b91cd7de6581d3ee0e020
AjitAdin <ajitadin03@gmail.com>
2025-02-22 01:22:39 +0530

    UPDT

--- TCSCODE/ARRAYS/src/Main.java
+++ TCSCODE/ARRAYS/src/Main.java
@@ -1,26 +1,45 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
+public class Main {
+    public static int thirdMax(int[] nums) {
+        Integer firstMax = null, secondMax = null, thirdMax = null;
 
-import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        String arr[]={"chiru","ajit","manju","ajit","akash","punith","manju"};
-        String temp[]=new String[arr.length];
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(temp));
-
-        for(int i=0;i<arr.length;i++){
-
-            for(int j=0;j<i;j++){
-                if(arr[i].equals(temp[j])){
-
-                    continue;
-                }
-
+        for (int num : nums) {
+            if (num == firstMax || num == secondMax || num == thirdMax) {
+                continue; // Skip duplicates
             }
-            temp[i]=arr[i];
-        }
-        System.out.println(Arrays.toString(temp));
 
+            if (firstMax == null || num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (secondMax == null || num > secondMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (thirdMax == null || num > thirdMax) {
+                thirdMax = num;
+            }
+        }
+
+        // Fix: Ensure no null values are unboxed
+        if (thirdMax != null) {
+            return thirdMax;
+        } else if (firstMax != null) {
+            return firstMax;
+        } else {
+            return 0; // Default return in case of an empty array
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {3, 2, 1};
+        System.out.println(thirdMax(arr1)); // Output: 1
+
+        int[] arr2 = {2, 2, 3, 1};
+        System.out.println(thirdMax(arr2)); // Output: 1
+
+        int[] arr3 = {1, 2}; // Less than 3 distinct numbers
+        System.out.println(thirdMax(arr3)); // Output: 2
+
+        int[] arr4 = {}; // Empty array
+        System.out.println(thirdMax(arr4)); // Output: 0
     }
 }
