b6021d6a89cf9db9adf4ef757ad0ee1da60c0988
AjitAdin <ajitadin03@gmail.com>
2025-02-25 23:32:10 +0530

    updt

--- MultiDimensionalArrays/src/SparseMatrixs.java
+++ MultiDimensionalArrays/src/SparseMatrixs.java
@@ -1,2 +1,35 @@
 public class SparseMatrixs {
+
+
+    public static void main(String atgs[]){
+        int matrix[][]={
+                {1,2,0,4},
+                {4,8,0,0},
+                {0,0,0,0}
+        };
+
+
+        int count_0=0;
+        int count_1=0;
+        int n=matrix.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                       if(matrix[i][j]==0){
+                           count_0++;
+                       }
+                       else{
+                           count_1++;
+                       }
+            }
+        }
+        if(count_0>count_1){
+            System.out.println("Sparse matrix");
+
+        }
+        else{
+            System.out.println("Not a Sparse mtrix ");
+
+        }
+    }
 }
