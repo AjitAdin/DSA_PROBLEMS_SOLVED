fbcf47c615dd630e601f54c2ba61a3f02349be28
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-04-27 10:14:28 +0530

    Update RemoveAllDuplicatesfromaString.java

--- javastrings/src/RemoveAllDuplicatesfromaString.java
+++ javastrings/src/RemoveAllDuplicatesfromaString.java
@@ -27,23 +27,69 @@ public class RemoveAllDuplicatesfromaString{
         StringBuilder result=new StringBuilder();
         for(int i=0;i<str.length();i++){
             boolean isDuplicate=false;
+            System.out.println("i= "+i);
             for(int j=0;j<i;j++){
+                
                 if(str.charAt(i)==str.charAt(j)){
+                    System.out.println(str.charAt(i)+" == "+str.charAt(j));
                     isDuplicate=true;
                     break;
                 }
-            }
+            }// 
 
             if(!isDuplicate){
                 result.append(str.charAt(i));
+                System.out.println("appended -- "+str.charAt(i));
             }
-        }
-        System.out.println(result.toString());
+        }//
+        // System.out.println(result.toString());//output
     }
 
     public static void main(String args[]){
 
-        String str="caabbdacdcbc";
+        String str="fcaabbdacdcbc";
         Remove(str);
     }
 }
+
+//method 2
+// class Main {
+//     public static void main(String[] args) {
+//        String s="abcbcadt";//abcdt
+//        boolean[] visited=new boolean[256];
+//        StringBuilder sb=new StringBuilder();
+       
+//        for(int i=0;i<s.length();i++){
+//            char ch=s.charAt(i);
+//            if(!visited[ch]){
+//                visited[ch]=true;
+//                sb.append(ch);
+//            }
+//        }
+//       System.out.println(sb.toString());
+//     }
+// }
+
+
+// You are dealing with Unicode characters (not just ASCII) — like emojis, special symbols, etc.
+//Automatically avoids duplicates — no need to manually check if an element already exists.
+// import java.util.HashSet;
+
+// class Main {
+//     public static void main(String[] args) {
+//         String s = "abcbcadt";
+//         HashSet<Character> seen = new HashSet<>();
+//         StringBuilder sb = new StringBuilder();
+        
+//         for (char ch : s.toCharArray()) {
+//             if (!seen.contains(ch)) {
+//                 seen.add(ch);
+//                 sb.append(ch);
+//             }
+//         }
+        
+//         System.out.println(sb.toString());
+//     }
+// }
+
+
