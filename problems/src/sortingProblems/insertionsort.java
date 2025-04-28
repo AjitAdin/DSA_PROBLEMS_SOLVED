7eb8549542c19d3e4d0c25501118c9651dce5ba4
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-04-28 17:13:01 +0530

    Update insertionsort.java

--- problems/src/sortingProblems/insertionsort.java
+++ problems/src/sortingProblems/insertionsort.java
@@ -2,29 +2,29 @@ package sortingProblems;
 
 public class insertionsort {
 
-	
-	static void sort(int arr[],int n){
-        for(int i=1;i<n;i++){
-            int temp=arr[i];
-            for(int j=(i-1);j>=0;j--){
-                if(temp<arr[j]){
-                    arr[j+1]=arr[j];
-                    arr[j]=temp;
-                }
+    static void sort(int arr[], int n) {
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            
+            // Shift elements to right until the correct position is found
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+            // Place temp at its correct position
+            arr[j + 1] = temp;
         }
         
-        for(int i=0;i<n;i++){
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
             System.out.println(arr[i]);
         }
     }
-    
-    
-    public static void main(String args[]){
-        int arr[]={23,1,10,5,2};
-        int n=arr.length;
-        sort(arr,n);
+
+    public static void main(String args[]) {
+        int arr[] = {23, 1, 10, 5, 2};
+        int n = arr.length;
+        sort(arr, n);
     }
-	
-	
 }
