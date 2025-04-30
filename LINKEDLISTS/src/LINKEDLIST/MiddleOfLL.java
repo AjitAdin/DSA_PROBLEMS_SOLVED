54a538633f352f6ce297c95014a9bbaaa6290907
AjitAdin <ajitadin03@gmail.com>
2025-04-30 21:58:31 +0530

    updt

--- LINKEDLISTS/src/LINKEDLIST/MiddleOfLL.java
+++ LINKEDLISTS/src/LINKEDLIST/MiddleOfLL.java
@@ -70,3 +70,20 @@ class MiddleOfLL {
 
     }
 }
+
+//fast and slow pointer technique
+//public class Solution {
+//    public ListNode middleNode(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head;
+//
+//        // Move fast two steps and slow one step
+//        while (fast != null && fast.next != null) {
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//
+//        // When fast reaches the end, slow is at the middle
+//        return slow;
+//    }
+//}
