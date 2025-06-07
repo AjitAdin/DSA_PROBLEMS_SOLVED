08edc7e7afccfb59a6e964cffad000cd9bd2c6a2
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-06-07 20:12:56 +0530

    Update CountNumberOfWords.java

--- javastrings/src/CountNumberOfWords.java
+++ javastrings/src/CountNumberOfWords.java
@@ -18,3 +18,28 @@ public class CountNumberOfWords {
         System.out.println(count(str));
     }
 }
+
+
+//  very very imp pattern 
+class Main {
+    public static void main(String[] args) {
+        String s = "                    HI    AMRMY  AND DEFENSE    hello";
+        s = s.trim();
+        
+        int count = 0;
+        boolean inWord = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                if (!inWord) {
+                    count++;
+                    inWord = true;
+                }
+            } else {
+                inWord = false;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
