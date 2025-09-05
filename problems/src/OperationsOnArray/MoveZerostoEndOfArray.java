7b033428fa01ae37908aa22ac3ccf339371830a5
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-09-05 23:44:46 +0530

    Update MoveZerostoEndOfArray.java

--- problems/src/OperationsOnArray/MoveZerostoEndOfArray.java
+++ problems/src/OperationsOnArray/MoveZerostoEndOfArray.java
@@ -42,3 +42,19 @@ public class MoveZerostoEndOfArray {
 	   display(arr,n);
    }
 }
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index=0
+        for i in range(len(nums)):
+            if nums[i]==0:
+                continue
+            else:
+                nums[index]=nums[i]
+                index+=1
+        for j in range(index,len(nums)):
+            nums[j]=0
