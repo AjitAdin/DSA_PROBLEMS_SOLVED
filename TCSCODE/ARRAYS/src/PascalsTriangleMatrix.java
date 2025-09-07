a71280ecfdc52d2f8cbeb3d9b09ebcc0444d423e
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-09-07 15:32:09 +0530

    Update PascalsTriangleMatrix.java

--- TCSCODE/ARRAYS/src/PascalsTriangleMatrix.java
+++ TCSCODE/ARRAYS/src/PascalsTriangleMatrix.java
@@ -35,3 +35,29 @@ class PascalsTriangleMatrix {
         }
     }
 }
+
+
+
+def pascals_triangle(n):
+    triangle = []  # list to hold rows of Pascal's triangle
+
+    for i in range(n):
+        row = [1] * (i + 1)  # each row starts and ends with 1
+        print("row:",row)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # sum of two numbers above
+        triangle.append(row)
+
+    return triangle
+
+
+# Example usage
+n = 6  # number of rows
+triangle = pascals_triangle(n)
+
+# # Pretty print the triangle
+# for row in triangle:
+#     print(" ".join(map(str, row)).center(n * 2))
+
+for i in triangle:
+    print(i)
