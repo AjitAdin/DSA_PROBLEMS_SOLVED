8bdb0646a573318b4e67e2b8c7a8dbb02f857286
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-09-07 11:00:33 +0530

    Update TwoSumTarget.java

--- TCSCODE/ARRAYS/src/TwoSumTarget.java
+++ TCSCODE/ARRAYS/src/TwoSumTarget.java
@@ -30,3 +30,12 @@ public class TwoSumTarget {
 //        return new int[] {}; // Return empty array if no solution found
 //    }
 //}
+
+
+//
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
