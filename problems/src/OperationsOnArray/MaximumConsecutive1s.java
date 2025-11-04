6f23b3c2dc173656e6cbebe655c1f5acdcf62327
AjitAdin <140914554+AjitAdin@users.noreply.github.com>
2025-11-05 03:12:00 +0530

    Update MaximumConsecutive1s.java

--- problems/src/OperationsOnArray/MaximumConsecutive1s.java
+++ problems/src/OperationsOnArray/MaximumConsecutive1s.java
@@ -52,3 +52,23 @@ public class MaximumConsecutive1s {
 	   Maximumones(arr,n);
   }
 }
+
+
+// 
+arr = [0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1]
+
+max_ones = 0
+count = 0
+
+for num in arr:
+    if num == 1:
+        count += 1
+        max_ones = max(max_ones, count)
+    else:
+        count = 0
+
+print(max_ones)
+
+
+
+	
